2d3114875435d419bb4c5d06a8c10afea7e264e3
thomaschiari <thomaschiari@gmail.com>
2022-09-28 10:51:13 -0300

    exercicio 3

--- componentes.py
+++ componentes.py
@@ -41,14 +41,30 @@ def exe2(p, q, r, s):
 
     return instances()
 
+'''
+def exe2(p, q, r, s):
+
+    condicao1 = Signal(bool(0))
+    condicao2 = Signal(bool(0))
+    
+    @always_comb
+    def comb():
+        condicao1 = not p
+        condicao2 = q and r
+
+        s.next = condicao1 or condicao2
+
+    return instances()
+'''
+
 
 def exe3(x1, x0, y1, y0, z3, z2, z1, z0):
     @always_comb
     def comb():
-        z0.next = 0
-        z1.next = 0
-        z2.next = 0
-        z3.next = 0
+        z0.next = ((not x1) and x0 and (not y1) and y0) or ((not x1) and x0 and y1 and y0) or (x1 and x0 and (not y1) and y0) or (x1 and x0 and y1 and y0)
+        z1.next = ((not x1) and x0 and y1 and (not y0)) or ((not x1) and x0 and y1 and y0) or (x1 and (not x0) and (not y1) and y0) or (x1 and (not x0) and y1 and y0)
+        z2.next = (x1 and (not x0) and y1 and (not y0)) or (x1 and (not x0) and y1 and y0) or (x1 and x0 and y1 and (not y0))
+        z3.next = x1 and x0 and y1 and y0
 
     return instances()
 
